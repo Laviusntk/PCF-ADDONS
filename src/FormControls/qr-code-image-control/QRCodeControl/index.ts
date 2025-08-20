06f457d070356a45868419e08250050c4c976096
Lavius Motileng <103624603+Laviusntk@users.noreply.github.com>
2025-08-20 19:08:27 +0200

    format index.ts

--- src/FormControls/qr-code-image-control/QRCodeControl/index.ts
+++ src/FormControls/qr-code-image-control/QRCodeControl/index.ts
@@ -18,7 +18,11 @@ export class QRCodeControl implements ComponentFramework.StandardControl<IInputs
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
      */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary,
+        container:HTMLDivElement): void
     {
         // Add control initialization code
     }
@@ -28,7 +32,8 @@ export class QRCodeControl implements ComponentFramework.StandardControl<IInputs
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
      */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
+    public updateView(
+        context: ComponentFramework.Context<IInputs>): void
     {
         // Add code to update control view
     }
